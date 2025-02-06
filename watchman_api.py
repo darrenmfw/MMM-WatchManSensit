987b3b467f238066a3301d535fa26f042927c152
darrenmfw <darrenmillington55@gmail.com>
2025-02-06 21:14:09 +0000

    Update watchman_api.py

--- watchman_api.py
+++ watchman_api.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, "/home/pi/MagicMirror/modules/MMM-WatchManSensit/venv/lib/python3.11/site-packages")
+
 from flask import Flask, jsonify
 from kingspan_connect_sensor import KingspanConnect
 import datetime
