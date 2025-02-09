c7c144eaa5b6bab6e4bc8d9e1aea0828b65d4a6a
darrenmfw <darrenmillington55@gmail.com>
2025-02-09 09:10:17 +0000

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -3,132 +3,134 @@ var https = require("https");
 var xml2js = require("xml2js");
 
 module.exports = NodeHelper.create({
+    // This function processes all tanks in the config.tanks array.
     updateLatestLevel: function(config) {
         var self = this;
-        // Construct the SOAP XML envelope.
-        // The user ID is constructed by prefixing "BOX" to the serialNumber,
-        // and the signalman number is simply the serialNumber.
-        var userId = "BOX" + config.serialNumber;
-        var signalmanNo = config.serialNumber;
-        
-        var soapEnvelope =
-          '<?xml version="1.0" encoding="utf-8"?>' +
-          '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-          'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-          'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
-            '<soap:Body>' +
-              '<SoapMobileAPPGetLatestLevel_v3 xmlns="http://mobileapp/">' +
-                '<userid>' + userId + '</userid>' +
-                '<password>' + config.password + '</password>' +
-                '<signalmanno>' + signalmanNo + '</signalmanno>' +
-                '<culture>' + config.culture + '</culture>' +
-              '</SoapMobileAPPGetLatestLevel_v3>' +
-            '</soap:Body>' +
-          '</soap:Envelope>';
+        var tanks = config.tanks; 
+        if (!tanks || !Array.isArray(tanks) || tanks.length === 0) {
+            self.sendSocketNotification("WATCHMAN_DATA_RESPONSE", []);
+            return;
+        }
+        // We'll store results in an array.
+        var results = [];
+        var completedRequests = 0;
 
-        var options = {
-            hostname: 'www.connectsensor.com',
-            path: '/soap/MobileApp.asmx',
-            method: 'POST',
-            headers: {
-                "Content-Type": "text/xml; charset=utf-8",
-                "SOAPAction": '"http://mobileapp/SoapMobileAPPGetLatestLevel_v3"',
-                "Content-Length": Buffer.byteLength(soapEnvelope)
-            }
-        };
+        // Process each tank individually.
+        tanks.slice(0, 3).forEach(function(tankConfig, index) {
+            // For each tank, construct the SOAP envelope.
+            // User ID is "BOX" + tankConfig.serialNumber and signalman number is tankConfig.serialNumber.
+            var userId = "BOX" + tankConfig.serialNumber;
+            var signalmanNo = tankConfig.serialNumber;
+            
+            var soapEnvelope =
+              '<?xml version="1.0" encoding="utf-8"?>' +
+              '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+              'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+              'xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+                '<soap:Body>' +
+                  '<SoapMobileAPPGetLatestLevel_v3 xmlns="http://mobileapp/">' +
+                    '<userid>' + userId + '</userid>' +
+                    '<password>' + config.password + '</password>' +
+                    '<signalmanno>' + signalmanNo + '</signalmanno>' +
+                    '<culture>' + config.culture + '</culture>' +
+                  '</SoapMobileAPPGetLatestLevel_v3>' +
+                '</soap:Body>' +
+              '</soap:Envelope>';
 
-        var req = https.request(options, function(res) {
-            var data = "";
-            res.on("data", function(chunk) {
-                data += chunk;
-            });
-            res.on("end", function() {
-                console.log("Received SOAP response:", data);
-                var parser = new xml2js.Parser({
-                    explicitArray: false,
-                    tagNameProcessors: [xml2js.processors.stripPrefix],
-                    ignoreAttrs: true
+            var options = {
+                hostname: 'www.connectsensor.com',
+                path: '/soap/MobileApp.asmx',
+                method: 'POST',
+                headers: {
+                    "Content-Type": "text/xml; charset=utf-8",
+                    "SOAPAction": '"http://mobileapp/SoapMobileAPPGetLatestLevel_v3"',
+                    "Content-Length": Buffer.byteLength(soapEnvelope)
+                }
+            };
+
+            var req = https.request(options, function(res) {
+                var data = "";
+                res.on("data", function(chunk) {
+                    data += chunk;
                 });
-                parser.parseString(data, function(err, result) {
-                    if (err) {
-                        self.sendSocketNotification("WATCHMAN_ERROR", "XML parse error: " + err);
-                    } else {
-                        console.log("Parsed XML object:", result);
-                        try {
-                            var envelope = result.Envelope;
-                            var body = envelope.Body;
-                            var response = body.SoapMobileAPPGetLatestLevel_v3Response;
-                            var resultData = response.SoapMobileAPPGetLatestLevel_v3Result;
-                            
-                            // Expecting a <Tanks> element containing one or more <APITankInfo_V3> entries.
-                            var tanksContainer = resultData.Tanks;
-                            if (tanksContainer && tanksContainer.APITankInfo_V3) {
-                                var tanks = tanksContainer.APITankInfo_V3;
-                                // Ensure tanks is an array.
-                                if (!Array.isArray(tanks)) {
-                                    tanks = [tanks];
-                                }
-                                // Limit to a maximum of three tanks.
-                                tanks = tanks.slice(0, 3);
-                                
-                                var sensors = [];
-                                tanks.forEach(function(tank) {
-                                    // Only process if there is valid data
-                                    if (tank && tank.LevelPercentage && tank.ReadingDate) {
-                                        var tankName = tank.TankName || "Unnamed Tank";
-                                        var fillLevel = tank.LevelPercentage;
-                                        var readingDate = tank.ReadingDate;
-                                        var runOutDate = tank.RunOutDate;
-                                        
-                                        // Format reading date (2-digit year, no seconds)
-                                        var d = new Date(readingDate);
-                                        var formattedReadingDate = d.toLocaleString("en-GB", {
+                res.on("end", function() {
+                    console.log("Received SOAP response for tank " + tankConfig.serialNumber + ":", data);
+                    var parser = new xml2js.Parser({
+                        explicitArray: false,
+                        tagNameProcessors: [xml2js.processors.stripPrefix],
+                        ignoreAttrs: true
+                    });
+                    parser.parseString(data, function(err, result) {
+                        if (err) {
+                            results[index] = { tankName: tankConfig.tankName, error: "XML parse error: " + err };
+                        } else {
+                            try {
+                                // Navigate the parsed XML structure.
+                                var envelope = result.Envelope;
+                                var body = envelope.Body;
+                                var response = body.SoapMobileAPPGetLatestLevel_v3Response;
+                                var resultData = response.SoapMobileAPPGetLatestLevel_v3Result;
+                                var levelElement = resultData.Level;
+                                if (levelElement && levelElement.LevelPercentage) {
+                                    var fillLevel = levelElement.LevelPercentage;
+                                    var readingDate = levelElement.ReadingDate;
+                                    var runOutDate = levelElement.RunOutDate;
+                                    
+                                    // Format the reading date (2-digit year, no seconds).
+                                    var d = new Date(readingDate);
+                                    var formattedReadingDate = d.toLocaleString("en-GB", {
+                                        year: '2-digit',
+                                        month: '2-digit',
+                                        day: '2-digit',
+                                        hour: '2-digit',
+                                        minute: '2-digit'
+                                    });
+                                    
+                                    // Format the run out date as date only (2-digit year) if valid.
+                                    var formattedRunOutDate = "";
+                                    if (runOutDate && runOutDate !== "0001-01-01T00:00:00") {
+                                        var dRun = new Date(runOutDate);
+                                        formattedRunOutDate = dRun.toLocaleDateString("en-GB", {
                                             year: '2-digit',
                                             month: '2-digit',
-                                            day: '2-digit',
-                                            hour: '2-digit',
-                                            minute: '2-digit'
-                                        });
-                                        
-                                        // Format run out date as date only (2-digit year)
-                                        var formattedRunOutDate = "";
-                                        if (runOutDate && runOutDate !== "0001-01-01T00:00:00") {
-                                            var dRun = new Date(runOutDate);
-                                            formattedRunOutDate = dRun.toLocaleDateString("en-GB", {
-                                                year: '2-digit',
-                                                month: '2-digit',
-                                                day: '2-digit'
-                                            });
-                                        }
-                                        
-                                        sensors.push({
-                                            tankName: tankName,
-                                            fillLevel: fillLevel + "%",
-                                            lastReadingDate: formattedReadingDate,
-                                            runOutDate: formattedRunOutDate,
-                                            rawRunOutDate: runOutDate
+                                            day: '2-digit'
                                         });
                                     }
-                                });
-                                self.sendSocketNotification("WATCHMAN_DATA_RESPONSE", sensors);
-                            } else {
-                                // No tanks data found.
-                                self.sendSocketNotification("WATCHMAN_DATA_RESPONSE", []);
+                                    
+                                    results[index] = {
+                                        tankName: tankConfig.tankName,
+                                        fillLevel: fillLevel + "%",
+                                        lastReadingDate: formattedReadingDate,
+                                        runOutDate: formattedRunOutDate,
+                                        rawRunOutDate: runOutDate
+                                    };
+                                } else {
+                                    results[index] = { tankName: tankConfig.tankName, error: "No valid level data" };
+                                }
+                            } catch (ex) {
+                                results[index] = { tankName: tankConfig.tankName, error: "Error extracting data: " + ex };
                             }
-                        } catch (ex) {
-                            self.sendSocketNotification("WATCHMAN_ERROR", "Error extracting data: " + ex);
                         }
-                    }
+                        completedRequests++;
+                        if (completedRequests === tanks.slice(0, 3).length) {
+                            // Send the aggregated results to the front-end.
+                            self.sendSocketNotification("WATCHMAN_DATA_RESPONSE", results);
+                        }
+                    });
                 });
             });
+            
+            req.on("error", function(err) {
+                results[index] = { tankName: tankConfig.tankName, error: "HTTP error: " + err };
+                completedRequests++;
+                if (completedRequests === tanks.slice(0, 3).length) {
+                    self.sendSocketNotification("WATCHMAN_DATA_RESPONSE", results);
+                }
+            });
+            
+            req.write(soapEnvelope);
+            req.end();
         });
-
-        req.on("error", function(err) {
-            self.sendSocketNotification("WATCHMAN_ERROR", "HTTP error: " + err);
-        });
-
-        req.write(soapEnvelope);
-        req.end();
     },
 
     socketNotificationReceived: function(notification, payload) {
